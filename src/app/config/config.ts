7963cf8194d740de40d21db23e7345974ffc782b
Benjamin Flores <benjamin.flores.r@gmail.com>
2020-07-16 11:48:38 -0600

    cfg

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,12 +1,2 @@
 
 
-// export const URL_SERVICIOS = 'http://ec2-3-21-106-87.us-east-2.compute.amazonaws.com/api/v1';
-
-// AMBIENTE DEMO
-// export const URL_SERVICIOS = 'http://ec2-3-21-106-87.us-east-2.compute.amazonaws.com/api/v1';
-// export const SECRET_KEY = 'd88577bea5ba12e079a5ccd3c2face64';
-
-
-// AMBIENTE DESARROLLO
- export const URL_SERVICIOS = 'http://ec2-3-128-155-64.us-east-2.compute.amazonaws.com/api/v1';
- export const SECRET_KEY = '71e141d3a016ffd6bd94558a5bb80b15';
